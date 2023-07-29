2da17d58bde02cc93ed9cbac4cd1fe1048d02836
Nirbhay Singh <nirbhaymahan100@gmail.com>
2023-07-29 17:41:25 +0530

    trial 2

---
+++ src/components/Contact.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Contact = () =>{
+    return (
+        <div>
+            <p className="mt-5">WELCOME</p>
+            <h1>We Are the World</h1>
+        </div>
+    )
+}
+
+export default Contact;
